c75bdbe4db2e229282df61b7e915671b926d0798
George Suarez <georgesuarezdev@gmail.com>
2025-07-24 17:24:35 -0700

    Initial Commit

---
+++ MyTodoApp/MyTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoAppApp.swift
+//  MyTodoApp
+//
+//  Created by George Suarez on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyTodoAppUITests/MyTodoAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MyTodoAppUITestsLaunchTests.swift
+//  MyTodoAppUITests
+//
+//  Created by George Suarez on 7/24/25.
+//
+
+import XCTest
+
+final class MyTodoAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
